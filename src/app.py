2af908ee852262b6729b2b80accabc138c7a1cbb
Caio Almeida <caio2003@outlook.pt>
2023-04-18 00:55:15 -0300

    correcoes da main, inclui FLASK funcionando e criacao de uma pasta para as pesquisas

--- src/app.py
+++ src/app.py
@@ -1,27 +1,14 @@
 from flask import Flask, render_template
-from jinja2 import Template
-import requests
-from bs4 import BeautifulSoup
-
 app = Flask(__name__)
 
-@app.route("/") 
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/pesquisa") 
+@app.route('/pesquisa')
 def pesquisa():
-    return render_template("pesquisa.html")
+    return render_template('pesquisa.html')
 
-@app.route("/sobreoprojeto") 
+@app.route('/sobre')
 def sobre():
-    return render_template("sobre_o_projeto.html")
-
-# Raspagem de dados
-vgm_url = 'https://taubate.meumunicipio.digital/apex/taubate/f?p=839:3:::::P3_COD_APLICACAO_COVID,P3_MES_INICIAL,P3_MES_FINAL,P3_QUEBRA,P3_TIPO_RECEITA,P3_PARAMETRO:312,1,12,3,O,1'
-html_text = requests.get(vgm_url).text
-soup = BeautifulSoup(html_text, 'html.parser')
-
-
-if __name__ == '__main__':
-    app.run()
+    return render_template('sobre.html')

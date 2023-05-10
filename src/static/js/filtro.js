3824a3567eef972b9bdb5056bf376b30acd56507
IagoSouza <iago.csouza15@gmail.com>
2023-05-10 18:18:08 -0300

    trocando div conteudo pelos dados buscados

--- src/static/js/filtro.js
+++ src/static/js/filtro.js
@@ -1,3 +1,14 @@
+/*
+result = {
+    tipos = [
+        {
+            tipo = 'vacinação',
+            dados = [{"ano":"2019","cidade":"Caçapava", ...}]
+        }
+    ]
+}
+*/
+
 function filtrar() {
     cidade = document.querySelector('#cidade').value
     tipo = document.querySelector('#topico').value
@@ -7,13 +18,15 @@ function filtrar() {
         if (this.readyState == 4 && this.status == 200) {
             result = JSON.parse(xhr.responseText)
             console.log(result)
+            document.getElementsByClassName('conteudo')[0].style.display = 'none'
+            document.getElementById('resultado').innerHTML = JSON.stringify(result.tipos)
         }
     };
     if (!cidade && !tipo) {
         alert('Preencha algum campo')
     } else {
         xhr.open('POST', 'http://localhost:5000/filtrar', true)
-        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        xhr.send(JSON.stringify(filtro));
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+        xhr.send(JSON.stringify(filtro))
     }
 }

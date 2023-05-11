427b0bb0691f72a9f56898eac33144d0a99a74da
leticiacarvalho04 <110743347+leticiacarvalho04@users.noreply.github.com>
2023-05-11 20:27:24 -0300

    Inseri os gráficos de vacinação
    Inseri os gráficos sem ligá-los ao resultado dos filtros

--- src/static/js/filtro.js
+++ src/static/js/filtro.js
@@ -19,17 +19,50 @@ function filtrar() {
         result = JSON.parse(xhr.responseText);
         document.getElementsByClassName('conteudo')[0].style.display = 'none';
         document.getElementById('resultado').innerHTML = JSON.stringify(result.tipos);
-        document.getElementById('resultado2').innerHTML = '<div class="chartCard"><div class="chartBox"><canvas id="myChart"></canvas></div></div>'
+        document.getElementById('resultado2').innerHTML = '<div class="chartCard"><div class="chartBox"><canvas id="myChart1"></canvas></div></div>'
+        document.getElementById('resultado3').innerHTML = '<div class="chartCard"><div class="chartBox"><canvas id="Chart2"></canvas></div></div>'
+        document.getElementById('resultado4').innerHTML = '<div class="chartCard"><div class="chartBox"><canvas id="Chart3"></canvas></div></div>'
+        document.getElementById('resultado5').innerHTML = '<div class="chartCard"><div class="chartBox"><canvas id="Chart4"></canvas></div></div>'
+        document.getElementById('resultado6').innerHTML = '<div class="chartCard"><div class="chartBox"><canvas id="Chart5"></canvas></div></div>'
+        document.getElementById('resultado7').innerHTML = '<div class="chartCard"><div class="chartBox"><canvas id="Chart6"></canvas></div></div>'
+    
         
-        const ctx = document.getElementById('myChart');
-        new Chart(ctx, {
+        
+        const ctx = document.getElementById('myChart1').getContext('2d');
+        // Crie as instâncias de gráfico
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Primeira dose', 'Segunda dose', '1° reforço', '2° reforço', '3° reforço', 'dose única', 'dose adicional'],
+                datasets: [{
+                    label: 'Caçapava total de doses(21/22)',
+                    data: [88.797, 82.072, 56.582, 27.148, 126, 3.021, 289],
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true
+                }
+              }
+            }
+          });
+
+        const ctx2 = document.getElementById('Chart2').getContext('2d');
+        new Chart(ctx2, {
           type: 'bar',
           data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['Primeira dose', 'Segunda dose', '1° reforço', '2° reforço', '3° reforço', 'dose única', 'dose adicional'],
             datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              borderWidth: 1
+              label: 'Caçapava 2021',
+              data: [77.814, 71.754, 20.022, 7 , 0, 2.464, 122],
+              borderWidth: 1,
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
             }]
           },
           options: {
@@ -40,6 +73,100 @@ function filtrar() {
             }
           }
         });
+
+        const ctx3 = document.getElementById('Chart3').getContext('2d');
+        new Chart(ctx3, {
+          type: 'bar',
+          data: {
+            labels: ['Primeira dose', 'Segunda dose', '1° reforço', '2° reforço', '3° reforço', 'dose única', 'dose adicional'],
+            datasets: [{
+              label: 'Caçapava 2022',
+              data: [10.983, 10.318 , 36.560, 27.141 , 126 , 557, 167],
+              borderWidth: 1,
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+
+        const ctx4 = document.getElementById('Chart4').getContext('2d');
+        new Chart(ctx4, {
+          type: 'bar',
+          data: {
+            labels: ['Primeira dose', 'Segunda dose', '1° reforço', '2° reforço', '3° reforço', 'dose única', 'dose adicional'],
+            datasets: [{
+              label: 'Jacareí total de doses(21/22)',
+              data: [212.768, 200.093, 142.607, 56.060 , 375, 6.059, 185],
+              borderWidth: 1,
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+
+        const ctx5 = document.getElementById('Chart5').getContext('2d');
+        new Chart(ctx5, {
+          type: 'bar',
+          data: {
+            labels: ['Primeira dose', 'Segunda dose', '1° reforço', '2° reforço', '3° reforço', 'dose única', 'dose adicional'],
+            datasets: [{
+              label: ' São José dos Campos total de doses(21/22)',
+              data: [656.346, 608.222, 414.390, 199.452 , 1.510, 18.645, 1.799],
+              borderWidth: 1,
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+
+        const ctx6 = document.getElementById('Chart6').getContext('2d');
+        new Chart(ctx6, {
+          type: 'bar',
+          data: {
+            labels: ['Primeira dose', 'Segunda dose', '1° reforço', '2° reforço', '3° reforço', 'dose única', 'dose adicional'],
+            datasets: [{
+              label: ' Taubaté total de doses(21/22)',
+              data: [280.873, 264.749, 178.166, 84.786 , 258, 9.171, 1.702],
+              borderWidth: 1,
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+
+        
       }
     };
     if (!cidade && !tipo) {
@@ -49,71 +176,4 @@ function filtrar() {
       xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
       xhr.send(JSON.stringify(filtro));
     }
-  
-    const charData = '../docs/Arquivos_CSV';
-  
-    // parse the file from CSV to JSON
-    d3.csv(charData).then(function (result) {
-      console.log(result);
-      const cost = [];
-      const profit = [];
-      const revenue = [];
-  
-      for (let i = 0; i < result.length; i++) {
-        cost.push(result[i].cost);
-        profit.push(result[i].cost);
-        revenue.push(result[i].cost);
-      }
-  
-      // setup 
-      const data = {
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        datasets: [{
-          label: 'Weekly Sales',
-          data: cost,
-          backgroundColor: [
-            'rgba(255, 26, 104, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(0, 0, 0, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 26, 104, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(0, 0, 0, 1)'
-          ],
-          borderWidth: 1
-        }]
-      };
-  
-      // config 
-      const config = {
-        type: 'bar',
-        data,
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      };
-  
-      // render init block
-      const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-      );
-  
-      // Instantly assign Chart.js version
-      const chartVersion = document.getElementById('chartVersion');
-      chartVersion.innerText = Chart.version;
-    });
 }
